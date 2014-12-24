13135084c42c0f73b93016b38df50e8b741da706
yodalee <lc85301@gmail.com>
2014-12-25 01:30:11 +0800

    modify testcase

--- test_agilentBin.py
+++ test_agilentBin.py
@@ -10,35 +10,21 @@ class TestImportFunctions(unittest.TestCase):
     def test_10point(self):
         """test example 1 of agilent doc
         """
-        time, data = importAgilentBin.readfile("testcase/points10.bin")
-        timeAns = 1.0e-9 * np.array([
-            -0.0828,
-            -0.0578,
-            -0.0328,
-            -0.0078,
-            0.0172,
-            0.0422,
-            0.0672,
-            0.0922,
-            0.1172,
-            0.1422])
+        time, data = importAgilentBin.readfile("testcase/points10.bin", 0)
 
+        timeAns = 1.0e-9 * np.array([
+            -0.0828, -0.0578, -0.0328, -0.0078, 0.0172,
+            0.0422, 0.0672, 0.0922, 0.1172, 0.1422])
         dataAns = np.array([
-            -0.0525,
-            -0.0511,
-            -0.0490,
-            -0.0370,
-            -0.0103,
-            0.0179,
-            0.0332,
-            0.0374,
-            0.0380,
-            0.0368])
+            -0.0525, -0.0511, -0.0490, -0.0370, -0.0103,
+            0.0179, 0.0332, 0.0374, 0.0380, 0.0368])
 
         self.assertIsNotNone(time, "time should not be None")
+        for i, j in zip(time, timeAns):
+            self.assertAlmostEqual(i, j, places=7)
         self.assertIsNotNone(data, "data should not be None")
-        self.assertAlmostEqual(time, timeAns)
-        self.assertAlmostEqual(data, dataAns)
+        for i, j in zip(data, dataAns):
+            self.assertAlmostEqual(i, j, places=4)
 
 if __name__ == '__main__':
     unittest.main()
